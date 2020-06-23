d266029304e6aae8b55c1245640e1124faa3d9fe
Minh Nguyen <minhnguyenxx@gmail.com>
2020-06-24 00:46:47 +0100

    Attempt to repro webpack beta 18 issue

---
+++ config-overrides.js
@@ -0,0 +1,27 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+// SKIP_PREFLIGHT_CHECK=true react-app-rewired start --scripts-version @duplotech/react-scripts
+
+module.exports = function override(config, env) {
+  config.plugins.unshift(
+    new ModuleFederationPlugin({
+      name: 'app-1',
+      library: { type: 'var', name: 'app1' },
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+      },
+      shared: [
+        '@material-ui/styles/esm/StylesProvider',
+        '@material-ui/styles/esm/useTheme/ThemeContext',
+        { react: { singleton: true } },
+        { 'react-dom': { singleton: true } },
+      ],
+    }),
+  );
+
+  // Disable runtimeChunk when producing `remoteEntry.js`
+  delete config.optimization.runtimeChunk;
+
+  return config;
+};
